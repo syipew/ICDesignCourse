72743b169aef485bf88c11d6b70c62fa4b6b4f3c
Syifaaa <syfw.azira27@students.amikom.ac.id>
2024-12-19 15:06:06 +0700

    Update RoleProfile_Activity.kt
    intent dari role profile ke paket course

--- app/src/main/java/com/example/icdesigncourse/RoleProfile_Activity.kt
+++ app/src/main/java/com/example/icdesigncourse/RoleProfile_Activity.kt
@@ -1,21 +1,30 @@
 package com.example.icdesigncourse
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
 
 class RoleProfile_Activity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_role_profile)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnNavigate = findViewById<Button>(R.id.button)
+        btnNavigate.setOnClickListener {
+            val intent = Intent(this, paket_course::class.java)
+            startActivity(intent)
         }
     }
 }
